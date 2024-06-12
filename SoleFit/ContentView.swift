11852d874d2b3f254e6cc4c892055ab62c1323a9
Connie <coke14190@gmail.com>
2024-06-12 22:14:40 +0800

    Initial Commit

---
+++ SoleFit/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SoleFit
+//
+//  Created by hocman on 12/6/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SoleFit/SoleFitApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoleFitApp.swift
+//  SoleFit
+//
+//  Created by hocman on 12/6/2024.
+//
+
+import SwiftUI
+
+@main
+struct SoleFitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
